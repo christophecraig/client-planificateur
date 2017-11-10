cc755022555966a386392d2988eb369acfeb162d
christophecraig <christophe.craig@viacesi.fr>
2017-11-10 11:16:32 +0100

    auto-perform ajax request on change in input

--- components/Resource.vue
+++ components/Resource.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="detailedResource">
-      {{resource.firstName}} {{resource.name}}
+    <input type="number" min="1" name="id_resource" v-model="resource_id">
+      {{resourceInfo}}
   </div>
 </template>
 
@@ -11,24 +12,46 @@ export default {
   name: "Resource",
   data() {
     return {
+      resource_id: 1,
       resource: {},
       errors: []
     };
   },
-  props: {
-
+  watch: {
+    // Chaque fois que resource_id est modifié, on exécute la fonction suivante qui va chercher en base la resource d'id correspondant
+    resource_id() {
+      axios
+        .get(
+          "http://192.168.0.50/~ccraig/planificateur/rest/resource/get/" +
+            this.resource_id
+        )
+        .then(response => {
+          console.log(response.data);
+          this.resource = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    }
   },
-  created() {
-    axios
-      .get("http://localhost/~ccraig/planificateur/rest/resource/get/40")
-      .then(response => {
-        console.log(response.data);
-        this.resource = response.data;
-      })
-      .catch(e => {
-        this.errors.push(e);
-      });
+  computed: {
+    // Permet de mettre à jour automatiquement l'affichage sans avoir à surveiller this.resource
+    resourceInfo() {
+      return this.resource.firstname + " " + this.resource.name;
+    }
   }
+  // created() {
+  //   console.log(this)
+  //   axios
+  //     .get("http://localhost/~ccraig/planificateur/rest/resource/get/" + this.resourceId)
+  //     .then(response => {
+  //       console.log(response.data);
+  //       this.resource = response.data;
+  //     })
+  //     .catch(e => {
+  //       this.errors.push(e);
+  //     });
+  // }
 };
 </script>
 
